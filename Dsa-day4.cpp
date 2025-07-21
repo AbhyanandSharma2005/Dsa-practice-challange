bf4f5515ed9f3f562f262cee69c2468c79190b3e
AbhyanandSharma2005 <163466061+AbhyanandSharma2005@users.noreply.github.com>
2025-07-21 17:20:22 +0530

    Code practice in cpp

---
+++ Dsa-day4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string result = "";
+        int count = 1;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (i == 0 || s[i] != s[i - 1]) {
+                count = 1;
+            } else {
+                count++;
+            }
+
+            if (count < 3) {
+                result += s[i];
+            }
+        }
+
+        return result;
+    }
+};
