023cd8197eeae0a076cec4cdedc8436943e62eea
AbhyanandSharma2005 <163466061+AbhyanandSharma2005@users.noreply.github.com>
2025-09-08 22:07:30 +0530

    Code Practice

---
+++ Dsa-day53.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for(int a = 1; a < n; ++a) {
+            int b = n - a;
+            string sa = to_string(a), sb = to_string(b);
+            if(sa.find('0') == string::npos && sb.find('0') == string::npos) return {a, b};
+        }
+        return {};
+    }
+};
