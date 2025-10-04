b891fb4103650b4cb74680530ddb5af2b0339be0
AbhyanandSharma2005 <163466061+AbhyanandSharma2005@users.noreply.github.com>
2025-10-04 18:44:19 +0530

    Code Practice

---
+++ Dsa-day79.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int max_area = 0;
+        
+        while (left < right) {
+            int current_height = std::min(height[left], height[right]);
+            int width = right - left;
+            int current_area = current_height * width;
+            
+            max_area = std::max(max_area, current_area);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return max_area;
+    }
+};
