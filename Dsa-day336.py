45ba4e2c6beedf8b5e709e6beaab7d46f5a3c1f1
AbhyanandSharma2005 <163466061+AbhyanandSharma2005@users.noreply.github.com>
2026-06-30 20:55:38 +0530

    code practice

---
+++ Dsa-day336.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        last_a = last_b = last_c = -1
+        result = 0
+        for i, char in enumerate(s):
+            if char == 'a':
+                last_a = i
+            elif char == 'b':
+                last_b = i
+            else:
+                last_c = i
+            if last_a != -1 and last_b != -1 and last_c != -1:
+                min_last = min(last_a, last_b, last_c)
+                result += min_last + 1
+        return result
