91307a771c51a775a8730c4770c3737fd1e81db5
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-11 23:17:55 +0530

    code practice

---
+++ Dsa-day374.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    int missingInteger(std::vector<int>& nums) {
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+        
+        while (true) {
+            bool found = false;
+            for (int num : nums) {
+                if (num == sum) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return sum;
+            }
+            sum++;
+        }
+    }
+};
