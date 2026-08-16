58c90a6db748872933eebac7428327658f8dbef0
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 02:57:12 +0530

    updates

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -74,18 +74,18 @@
 //     }
 // };
 
-#include <vector>
-#include <cmath>
+// #include <vector>
+// #include <cmath>
 
-class Solution {
-public:
-    bool stoneGameIX(std::vector<int>& stones) {
-        int c0 = 0, c1 = 0, c2 = 0;
+// class Solution {
+// public:
+//     bool stoneGameIX(std::vector<int>& stones) {
+//         int c0 = 0, c1 = 0, c2 = 0;
         
-        for (int x : stones) {
-            (x % 3 == 0) ? c0++ : (x % 3 == 1) ? c1++ : c2++;
-        }
+//         for (int x : stones) {
+//             (x % 3 == 0) ? c0++ : (x % 3 == 1) ? c1++ : c2++;
+//         }
         
-        return (c0 & 1) ? std::abs(c1 - c2) > 2 : (c1 && c2);
-    }
-};
+//         return (c0 & 1) ? std::abs(c1 - c2) > 2 : (c1 && c2);
+//     }
+// };
