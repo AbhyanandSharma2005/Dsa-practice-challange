8fd6be2edcef467800032ad647aa815ba38e3ecd
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 02:53:03 +0530

    updated

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -4,12 +4,16 @@
 class Solution {
 public:
     bool stoneGameIX(std::vector<int>& stones) {
-        int c0 = 0, c1 = 0, c2 = 0;
+        int counts[3] = {0, 0, 0};
         
-        for (int x : stones) {
-            (x % 3 == 0) ? c0++ : (x % 3 == 1) ? c1++ : c2++;
+        for (int stone : stones) {
+            counts[stone % 3]++;
         }
         
-        return (c0 & 1) ? std::abs(c1 - c2) > 2 : (c1 && c2);
+        if (counts[0] % 2 == 0) {
+            return counts[1] > 0 && counts[2] > 0;
+        } else {
+            return std::abs(counts[1] - counts[2]) > 2;
+        }
     }
 };
