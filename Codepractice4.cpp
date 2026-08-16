5833875bab12394e6cca2406dfd8a18a7fbb4909
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 03:03:30 +0530

    updates

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -67,8 +67,3 @@ public:
         }
         
         if (counts[0] % 2 == 0) {
-            return counts[1] > 0 && counts[2] > 0;
-        } else {
-            return std::abs(counts[1] - counts[2]) > 2;
-        }
-    }
