f263789a7d9649d5bffcdd689af45f3b65277afb
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 02:52:34 +0530

    updated

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -1,35 +1,15 @@
 #include <vector>
-#include <algorithm>
+#include <cmath>
 
 class Solution {
-private:
-    bool canAliceWin(int c0, int c1, int c2) {
-        if (c1 == 0) return false; 
-        
-        int k = std::min(c1 - 1, c2); 
-        int played_non_zero = 1 + 2 * k; 
-        
-        if (c1 - 1 > k) {
-            played_non_zero++;
-        }
-        
-        int total_played = played_non_zero + c0; 
-        
-        if (total_played == c0 + c1 + c2) {
-            return false; 
-        }
-        
-        return total_played % 2 != 0; 
-    }
-    
 public:
     bool stoneGameIX(std::vector<int>& stones) {
-        int counts[3] = {0, 0, 0};
-        for (int stone : stones) {
-            counts[stone % 3]++;
+        int c0 = 0, c1 = 0, c2 = 0;
+        
+        for (int x : stones) {
+            (x % 3 == 0) ? c0++ : (x % 3 == 1) ? c1++ : c2++;
         }
         
-        return canAliceWin(counts[0], counts[1], counts[2]) || 
-               canAliceWin(counts[0], counts[2], counts[1]);
+        return (c0 & 1) ? std::abs(c1 - c2) > 2 : (c1 && c2);
     }
 };
