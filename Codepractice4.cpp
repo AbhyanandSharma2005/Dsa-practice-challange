23c77ac8aa4ccbb2101b7a59641854b0f5163adf
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 03:07:10 +0530

    updates

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -7,8 +7,3 @@ public:
         int counts[3] = {0, 0, 0};
         
         for (int stone : stones) {
-            counts[stone % 3]++;
-        }
-        
-        if (counts[0] % 2 == 0) {
-            return counts[1] > 0 && counts[2] > 0;
