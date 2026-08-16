07a31179cb5b8c1ad7f1c09a27920a76f986c2e2
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-17 03:01:11 +0530

    updates

--- Codepractice4.cpp
+++ Codepractice4.cpp
@@ -63,11 +63,11 @@ public:
         int counts[3] = {0, 0, 0};
         
         for (int stone : stones) {
-//             counts[stone % 3]++;
-//         }
+            counts[stone % 3]++;
+        }
         
-//         if (counts[0] % 2 == 0) {
-//             return counts[1] > 0 && counts[2] > 0;
+        if (counts[0] % 2 == 0) {
+            return counts[1] > 0 && counts[2] > 0;
 //         } else {
 //             return std::abs(counts[1] - counts[2]) > 2;
 //         }
