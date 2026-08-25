2bc72a754a7e3dce2127d58106a4693d39a3432e
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-25 23:54:18 +0530

    code practice

---
+++ Codepractice21.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        vector<long long> a(n);
+        for(int i=0;i<n;i++) scanf("%lld", &a[i]);
+        long long f = 0;
+        long long count = 0;
+        for(int i=n-1;i>=0;i--){
+            f = a[i] + max(f, 0LL);
+            if(f > 0) count++;
+        }
+        printf("%lld\n", count);
+    }
+    return 0;
+}
