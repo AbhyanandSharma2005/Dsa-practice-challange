6801d43d113de2585c628c1551ae4f1a86dfabf9
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-27 00:03:02 +0530

    code practice

---
+++ Codepractice22.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        num_set = set(nums)
+        target = k
+        while target in num_set:
+            target += k
+        return target
